953883d99210f41b91b38954eb815c7462037daf
Christian Ruales <christianpaulruales@gmail.com>
2020-05-17 23:08:56 -0500

    Pixelar y cambiar tamano a imagenes

---
+++ resizedImageBW.m
@@ -0,0 +1,11 @@
+function y1 = resizedImageBW(name)
+img1 = imread(name);
+grayImage = rgb2gray(img1);
+%imshow(grayImage)
+bw = im2bw(grayImage);
+resized=imresize(bw,[40 30]);
+imshow(resized);
+
+reshaped = reshape(resized,1200, 1);
+y1 = transpose(reshaped);
+end
